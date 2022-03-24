ae1ba6f6cff9e79e1d5f9837acc2ade4850eb858
RedstoneBoy0509 <jacob.rutledge0509@gmail.com>
2022-03-24 17:49:29 -0500

    Initial commit

---
+++ src/main/java/io/github/red050911/mod/feassistlib/FEAssistLib.java
@@ -0,0 +1,41 @@
+package io.github.red050911.mod.feassistlib;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(FEAssistLib.MOD_ID)
+public class FEAssistLib {
+
+    public static final String MOD_ID = "rfea";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public FEAssistLib() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modBus.addListener(this::setup);
+        modBus.addListener(this::enqueueIMC);
+        modBus.addListener(this::processIMC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+
+    }
+
+}
